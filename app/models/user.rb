18e88f6bf4bbe96d94e10392018109c9e979f304
Brodrick <brodrick.childs@gmail.com>
2013-03-11 18:10:25 -0400

    Seriously though.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save :default_values
+  #before_save :default_values
   attr_accessible :name, :oauth_expires_at, :oauth_token, :provider, :uid, :latest_stage, :score
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
@@ -8,13 +8,15 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.score = 0
+      user.latest_stage = 1
       user.save!
     end
   end
 
 private
   def default_values
-    score ||= 0;
-    latest_stage ||= 1;
+    self.score ||= 0;
+    self.latest_stage ||= 1;
   end
 end
