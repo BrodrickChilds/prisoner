b96dca7e07d8ee6f18af9b973d20fb1641b0d1c6
samsinai <sinai.sam@gmail.com>
2013-03-19 17:24:19 -0400

    removedbootstrap

--- Gemfile
+++ Gemfile
@@ -18,6 +18,9 @@ end
 gem 'omniauth-facebook'
 gem 'haml-rails'
 
+gem "therubyracer"
+gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
+gem "twitter-bootstrap-rails"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,11 @@
 //= require jquery_ujs
 //=require jquery-ui
 //= require_tree .
-
+//= require twitter/bootstrap/bootstrap-transition
+//= require twitter/bootstrap/bootstrap-alert
+//= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-button
+//= require twitter/bootstrap/bootstrap-collapse
 $(document).ready(function(){
 	$(function() {
 		$( '.btn').button();
