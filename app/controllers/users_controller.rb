5e09bf589c2db18d3fdfe92a8603f1b9a73e9f86
Brodrick <brodrick.childs@gmail.com>
2013-04-17 23:27:13 -0400

    Fix tokenizer bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
     friend_ids = current_user.friend_ids(graph)
-    @friends = User.where("name like ?", "%#{params[:q]}%").where(:id => friend_ids)
+    if Rails.env.production?
+      @friends = User.where("name ilike ?", "%#{params[:q]}%").where(:id => friend_ids)
+    else
+      @friends = User.where("name like ?", "%#{params[:q]}%").where(:id => friend_ids)
+    end
     respond_to do |format|
       format.html
       format.json { render :json => @friends.map(&:attributes) }
