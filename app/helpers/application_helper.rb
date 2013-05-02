419adab47635b29bef98976756932436d3c55b0b
Brodrick Childs <brodrick@mit.edu>
2013-05-02 17:25:15 -0400

    Fixed some sorting stuff on leaderboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def sortable(column, title = nil)  
+    title ||= column.titleize  
+    direction = (column == params[:sort] && params[:order] == "asc") ? "desc" : "asc"  
+    link_to title, :sort => column, :order => direction  
+  end 
 end
